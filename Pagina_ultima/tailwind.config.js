b9871920a8eb25bf0f79a7890a4a83c4ed1d8edd
asinomas <51768083+asinomas@users.noreply.github.com>
2026-01-22 00:55:42 -0300

    Update tailwind.config.js

--- Pagina_ultima/tailwind.config.js
+++ Pagina_ultima/tailwind.config.js
@@ -2,14 +2,10 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#000000",
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
