bdf8d572ac3d2bcff289b2c0f017e83deebeab22
asinomas <51768083+asinomas@users.noreply.github.com>
2026-01-21 22:37:22 -0300

    Update Logos.tsx

--- Pagina con modificaciones_ultima/components/Logos.tsx
+++ Pagina con modificaciones_ultima/components/Logos.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 const Logos: React.FC = () => {
   const logos = [
-    { name: 'Grupo Sura', src: 'https://lh3.googleusercontent.com/aida-public/AB6AXuC25COy7Low0wHC8VzLIcnEqFOv1a4pW7o6Gia3JwE4iOaVKktDfSGBPwlKNcJHcFyPUcFUh87hQGHrhS2q_Zf4dWCeW4f_va0CO5UkDapWQao6kEaGr5iI0W-hLn5vhliTwELXXkBoO8t_Nbv1raUtQtF8vdKKXBaPcAapxgOBjlh3bLDxXiIWhLU2mzs25knNrNtk4YW1TUK5Sb9XqueYM-LL1w7Fs7ZhEPMLl6r-IDDnBN3UEpMRiiMBi8MXSjxsZezSw9OKBdE' },
-    { name: 'Casa&Ideas', src: 'https://lh3.googleusercontent.com/aida-public/AB6AXuB_PR3PiwHCezgpRYUlPoR4MlsEMaQWdEYyPH96_F_wUKFiiR1VzSzRGavtV_5k2DBcIxwI-snSdcsIFw4DUDyg12YPj8ZXILScOr1Ec-LzKPhOunv7qfxU7F8MJO6QLpjZpsoyhEtrHBQcX4IjzF15mnwcAIjxDONTU9q7a08xQoC1qTAyLJk4yulqmko7wVuPU1bCAetKpU6sCaivIIt7diKlRk5CEOHSwuwgP93BWLLX9Q5J2e6lYxJX41-8szwMHAdnH-Cnx88' },
-    { name: 'Globant', src: 'https://lh3.googleusercontent.com/aida-public/AB6AXuChl8wrgb1JseDf17WTzv6Mk-REesFCHJB_t-75RG8x7BGPbAOKdK7SQ7qbwcy_VQiI_Pm-6vc_xMiWekwcMyhbQ7bbOKmaGNLFzk1eJ7s6sLcfnZyoGKuVKEEcbMfXkPg6Quvi_XYZVIwl-NjdthzSM6iCPh4Ppq2YVoJmDdjPAbyhfkkXWZXpXPv1hsRL_DAefsQb--hIfXwjc1Lo7u0jtLut6hCEYKYjq9R0XKI027272FG8X4grb7O8rxmCesIIrb_5UJzTxdo' },
-    { name: 'Marubeni', src: 'https://lh3.googleusercontent.com/aida-public/AB6AXuBrGNUT01l514xwYHwInp_zr42-hd_10WrpbFbJfVfXzMYRYBe6rW6toknvlzEqGSXxGGFLe7TdO2dqnPCplar-keoC03lmCNpjzJy5kN7oym_DukcZcpRL1N1GO2pza7HiVEV2lXsPGiaRfzaA31TK2wpDV_ZQLfx2eqTYbPw2opDg5t_gqIgh946NHaeC8okUGv1xt8SYMPJ2c-cCDJNg_i3iZiXqZEEFWXBplb_URxm5G0Q3lAoatdJk3-TOA4ZjbGq0_5zjorI' }
+    { name: 'Grupo Sura', src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Grupo_Sura_logo.svg/1280px-Grupo_Sura_logo.svg.png' },
+    { name: 'Casa&Ideas', src: 'https://static.wikia.nocookie.net/logopedia/images/0/01/Casaideas_logo.png/revision/latest?cb=20220120020452' },
+    { name: 'Globant', src: 'https://upload.wikimedia.org/wikipedia/commons/5/56/Globant-LightBG-Color%403x.png' },
+    { name: 'Marubeni', src: 'https://companieslogo.com/img/orig/8002.T_BIG-8c9cf180.png?t=1720244490' }
+    { name: 'Everis', src: 'https://iconape.com/wp-content/png_logo_vector/everis-ntt-data-logo.png' }
+    { name: 'Compunet', src: 'https://www.compunetgroup.net/logo-compunet.png' }
+  
   ];
 
   return (
