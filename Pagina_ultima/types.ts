cedb010663725ea7faa96c97a3414336ee95a82d
asinomas <51768083+asinomas@users.noreply.github.com>
2026-01-23 05:13:19 -0300

    Update types.ts

--- Pagina_ultima/types.ts
+++ Pagina_ultima/types.ts
@@ -1,1 +1,12 @@
-
+export interface OfficeLocation {
+  id: string;
+  city: string;
+  country: string;
+  /**
+   * Formato: [Longitud, Latitud]
+   * Es vital para que el mapa D3 ubique el punto correctamente.
+   */
+  coordinates: [number, number];
+  address: string;
+  type: 'Headquarters' | 'Regional Office' | 'Satellite';
+}
