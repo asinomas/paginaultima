e034acc35caebd1bc32c54399f2a40b6f5fd08fb
asinomas <51768083+asinomas@users.noreply.github.com>
2026-01-23 19:36:32 -0300

    Update App.tsx

--- Pagina_ultima/src/App.tsx
+++ Pagina_ultima/src/App.tsx
@@ -1,41 +1,17 @@
-// src/App.tsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Privacy from "./components/Privacy";
-import Terms from "./components/Terms";
+import Footer from "./components/Footer";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div className="app">
-        <Navbar />
-
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Hero />
-                <Services />
-                <About />
-                <Contact />
-              </>
-            }
-          />
-          <Route path="/privacy" element={<Privacy />} />
-          <Route path="/terms" element={<Terms />} />
-        </Routes>
-
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <Services />
+      <Footer />
+    </div>
   );
 };
 
