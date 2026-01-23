7902d95529552102989e7dede0bf98cd10eb9e9c
asinomas <51768083+asinomas@users.noreply.github.com>
2026-01-23 15:08:48 -0300

    Update ContactDetail.tsx

--- Pagina_ultima/src/components/ContactDetail.tsx
+++ Pagina_ultima/src/components/ContactDetail.tsx
@@ -34,7 +34,7 @@ const ContactDetail: React.FC = () => {
             
             <h2 className="text-5xl lg:text-6xl font-black text-slate-900 mb-8 tracking-tighter leading-tight">
               Diseñemos el <br />
-              <span className="text-[#135bec]">Futuro.</span>
+              <span className="text-[#135bec] italic">Futuro.</span>
             </h2>
             
             <p className="text-slate-500 text-lg mb-12 max-w-md leading-relaxed">
