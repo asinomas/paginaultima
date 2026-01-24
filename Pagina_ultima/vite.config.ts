110986255d330c457a9f7f10c5668bf973c06a51
asinomas <51768083+asinomas@users.noreply.github.com>
2026-01-23 21:13:30 -0300

    Update vite.config.ts

--- Pagina_ultima/vite.config.ts
+++ Pagina_ultima/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Esto asegura que las rutas de CSS y JS sean relativas y no fallen en Vercel
+  base: './', 
   build: {
     outDir: 'dist',
   }
