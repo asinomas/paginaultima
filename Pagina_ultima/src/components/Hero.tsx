2a415ff45cd17b73de924145c554d3dd215969e6
asinomas <51768083+asinomas@users.noreply.github.com>
2026-01-26 20:16:56 -0300

    Update Hero.tsx

--- Pagina_ultima/src/components/Hero.tsx
+++ Pagina_ultima/src/components/Hero.tsx
@@ -25,7 +25,7 @@ const Hero: React.FC<HeroProps> = ({ onNavigate }) => {
         {/* Aquí ajusto la distancia entre texto e imagen con gap */}
         <div className="max-w-7xl mx-auto flex items-center justify-center gap-4">
           
-          <div className={`max-w-4xl text-center transition-all duration-[3s] ${showImage ? '-translate-x-[55px]' : 'translate-x-0'}`}>
+          <div className={`max-w-4xl text-center transition-all duration-[3s] ${showImage ? '-translate-x-[70px]' : 'translate-x-0'}`}>
             
             <div className="inline-flex items-center gap-2 px-3 py-1 rounded-full bg-[#135bec]/10 border border-[#135bec]/20 mb-8">
               <span className="relative flex h-2 w-2">
