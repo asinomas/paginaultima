b7082fd24f2990f4a451f67ae0c6567e64522c42
asinomas <51768083+asinomas@users.noreply.github.com>
2026-01-25 23:12:38 -0300

    Update ServicesDetail.tsx

--- Pagina_ultima/src/components/ServicesDetail.tsx
+++ Pagina_ultima/src/components/ServicesDetail.tsx
@@ -103,7 +103,7 @@ const ServicesDetail: React.FC<ServicesDetailProps> = ({ onContactClick = () => 
               </h1>
 
               <p className="text-lg lg:text-xl text-slate-400 leading-relaxed max-w-2xl mb-10">
-                En BlackTI ayudamos a empresas a innovar, optimizar procesos y generar resultados medibles. Nuestra experiencia tecnológica se traduce en soluciones para lograr que su empresa escale en el entorno moderno.
+                En BlackTI ayudamos a empresas a innovar, optimizar procesos y generar resultados medibles. Nuestra experiencia se traduce en soluciones para lograr que su empresa escale en el entorno moderno.
               </p>
 
               <button
