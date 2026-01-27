ea3f557b8ffd35807f9d63f56cbdf58d9298f8aa
asinomas <51768083+asinomas@users.noreply.github.com>
2026-01-26 22:03:35 -0300

    Update Hero.tsx

--- Pagina_ultima/src/components/Hero.tsx
+++ Pagina_ultima/src/components/Hero.tsx
@@ -86,9 +86,7 @@ const Hero: React.FC<HeroProps> = ({ onNavigate }) => {
               src="/images/foto-hero.jpg" 
               alt="Hero" 
               className="w-full h-full object-cover scale-[1.5]
-               brightness-[0.5]
                contrast-[1.1]
-               saturate-[0.5]
               "
             />
           </div>
