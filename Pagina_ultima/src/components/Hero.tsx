8733f73efab2c10cd15986b3bcc5f201c321f395
asinomas <51768083+asinomas@users.noreply.github.com>
2026-01-28 03:55:25 -0300

    Update Hero.tsx

--- Pagina_ultima/src/components/Hero.tsx
+++ Pagina_ultima/src/components/Hero.tsx
@@ -46,9 +46,8 @@ const Hero: React.FC<HeroProps> = ({ onNavigate }) => {
                 </span>{' '}
                 
               <span className="text-5xl md:text-7xl font-bold text-white/95 tracking-tight mb-8 leading-[1.1] selection:bg-[#135bec]/30"> </span>
-                para {' '} tu Empresa
-                <span className="relative inline-block italic text-[#135bec] drop-shadow-[0_0_15px_rgba(19,91,236,0.3)] filter transition-all duration-700">
-                  Impulsar
+                para {' '} <span className="relative inline-block italic text-[#135bec] drop-shadow-[0_0_15px_rgba(19,91,236,0.3)] filter transition-all duration-700"> tu Empresa
+                  Impulsar tu Empresa
                 </span>{' '}
               </h1>
 
