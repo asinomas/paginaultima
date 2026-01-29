64ac5bf3b6598483235461d93e08e766c139f96a
asinomas <51768083+asinomas@users.noreply.github.com>
2026-01-29 19:01:47 -0300

    Update Hero.tsx

--- Pagina_ultima/src/components/Hero.tsx
+++ Pagina_ultima/src/components/Hero.tsx
@@ -144,7 +144,7 @@ const Hero: React.FC<HeroProps> = ({ onNavigate }) => {
                 duration: 2, 
                 ease: 'easeInOut'
               }}
-              className="hidden lg:block absolute right-0 -top-8 w-[339px] h-[500px] flex-shrink-0 overflow-hidden rounded-[20%_3%_20%_3%]"
+              className="hidden lg:block absolute right-0 -top-8 w-[339px] h-[400px] flex-shrink-0 overflow-hidden rounded-[20%_3%_20%_3%]"
             >
               <img
                 src="./images/foto-hero.jpg"
