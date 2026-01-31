a9ab9b80217cb07758374dfd01c8cd9e1f3315cf
asinomas <51768083+asinomas@users.noreply.github.com>
2026-01-31 20:04:40 -0300

    Update AboutDetail.tsx

--- Pagina_ultima/src/components/AboutDetail.tsx
+++ Pagina_ultima/src/components/AboutDetail.tsx
@@ -246,7 +246,7 @@ const AboutDetail: React.FC<AboutDetailProps> = ({ onContactClick, onNavigate })
 
                         {/* Descripción centrada */}
                         <div className="flex-1 flex items-center justify-center px-2">
-                          <p className="text-slate-700 text-sm leading-relaxed text-left">
+                          <p className="text-slate-700 text-sm leading-tight text-center">
                             {member.bio}
                           </p>
                         </div>
