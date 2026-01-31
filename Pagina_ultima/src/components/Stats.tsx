ab44e76a2d27e375d6078af9bbce780e12fe6dd1
asinomas <51768083+asinomas@users.noreply.github.com>
2026-01-31 20:16:07 -0300

    Update Stats.tsx

--- Pagina_ultima/src/components/Stats.tsx
+++ Pagina_ultima/src/components/Stats.tsx
@@ -19,7 +19,7 @@ const Stats: React.FC = () => {
       icon: 'tune',
       value: '100%',
       label: 'A Medida',
-      text: 'Diseñamos el perfil según el contexto y objetivos, reduciendo tiempos de onboarding.'
+      text: 'Creamos un perfil según el contexto y objetivos, reduciendo tiempos de onboarding.'
     }
   ];
 
