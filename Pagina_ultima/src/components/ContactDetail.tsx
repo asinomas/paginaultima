8737693513b2ccc27161eac9306b10b40e385497
asinomas <51768083+asinomas@users.noreply.github.com>
2026-02-02 22:36:58 -0300

    Update ContactDetail.tsx

--- Pagina_ultima/src/components/ContactDetail.tsx
+++ Pagina_ultima/src/components/ContactDetail.tsx
@@ -41,7 +41,7 @@ const ContactDetail: React.FC = () => {
             <div 
               className="absolute inset-0 z-0"
               style={{
-                backgroundImage: '.public/images/contact.webp',
+               backgroundImage: "url('/contact.webp')",
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
                 backgroundRepeat: 'no-repeat',
