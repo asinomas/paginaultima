b64eafd5c17c5af716501d2d76e5072fc36c2f97
asinomas <51768083+asinomas@users.noreply.github.com>
2026-02-04 18:15:06 -0300

    Update Services.tsx

--- Pagina_ultima/src/components/Services.tsx
+++ Pagina_ultima/src/components/Services.tsx
@@ -47,7 +47,7 @@ const HighLevelConsulting: React.FC = () => {
           <div className="flex flex-col">
             {/* Collaboration Models Section */}
             <div className="mb-12">
-              <h4 className="text-[11px] font-bold tracking-[0.2em] bg-slate-300 uppercase mb-4 block">
+              <h4 className="text-[11px] font-bold tracking-[0.2em] bg-[#135bec] uppercase mb-4 block">
                 Modelos de Colaboración
               </h4>
               {/* Línea horizontal debajo del título */}
