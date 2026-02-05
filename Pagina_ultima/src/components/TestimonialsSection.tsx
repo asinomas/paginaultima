a157a5528b1dfdbc785aa6671107ecb9ab7b4b8d
asinomas <51768083+asinomas@users.noreply.github.com>
2026-02-05 01:10:26 -0300

    Update TestimonialsSection.tsx

--- Pagina_ultima/src/components/TestimonialsSection.tsx
+++ Pagina_ultima/src/components/TestimonialsSection.tsx
@@ -68,7 +68,7 @@ const TestimonialsSection: React.FC = () => {
 
       {/* Contenido */}
       <div className="relative z-10 container mx-auto max-w-6xl px-6 py-24">
-        <div className="relative flex items-center justify-center h-[70vh]">
+        <div className="relative flex items-center justify-center h-[50vh]">
         
           
           {/* Botón anterior - lado izquierdo */}
