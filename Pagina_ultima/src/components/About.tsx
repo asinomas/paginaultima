8ddf58b6bc2707d34c75a3a16003d09e757acf45
asinomas <51768083+asinomas@users.noreply.github.com>
2026-02-05 01:40:33 -0300

    Update About.tsx

--- Pagina_ultima/src/components/About.tsx
+++ Pagina_ultima/src/components/About.tsx
@@ -12,7 +12,7 @@ const About: React.FC = () => {
             <div className="w-full flex justify-center relative hidden md:flex shadow-xl border border-slate-100">
               <img 
                 src="./images/team-talk-office.webp" 
-                className="h-[385px] w-full object-contain transition-transform duration-[1.5s] group-hover:scale-105 rounded-[2.5rem] blur-[0.7px]"
+                className="h-[450px] w-full object-contain transition-transform duration-[1.5s] group-hover:scale-105 rounded-[2.5rem] blur-[0.7px]"
                 alt="Grupo de oficina en reunión"
                 loading="lazy"
               />
